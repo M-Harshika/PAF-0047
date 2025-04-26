91aa4e24b89b40f38d03c3894be4010d49e4e83c
Harshika Maheswaran <IT22064554@my.sliit.lk>
2025-04-26 08:24:51 +0530

    learningplanrepo

--- PAF-0047/Backend/src/main/java/com/example/Backend/repository/LearningPlanRepository.java
+++ PAF-0047/Backend/src/main/java/com/example/Backend/repository/LearningPlanRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
 @Repository
 public interface LearningPlanRepository extends MongoRepository<LearningPlan, String> {
     // Find all learning plans ordered by creation date descending
